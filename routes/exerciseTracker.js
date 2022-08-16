84bcd122c84a4c5d5590679758cafe905dd1c949
jvitoralb <jvvalb@gmail.com>
2022-08-15 22:37:15 -0300

    added post and get to /api/users

--- routes/exerciseTracker.js
+++ routes/exerciseTracker.js
@@ -7,4 +7,26 @@ exerciseRouter.get('/', (req, res) => {
     res.sendFile(`${__dirname}/frontend/public/exercisetracker.html`);
 });
 
+exerciseRouter.get('/api/users', (req, res) => {
+    res.send([
+        {
+            username: 'User1',
+            _id: 'User1 id'
+        },
+        {
+            username: 'User2',
+            _id: 'User2 id'
+        }
+    ]);
+});
+
+exerciseRouter.post('/api/users', (req, res) => {
+    const {username} = req.body;
+
+    res.json({
+        username: username,
+        _id: 'its id'
+    });
+});
+
 export default exerciseRouter;
