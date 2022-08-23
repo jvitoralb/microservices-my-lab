bdd64faa657b1d3d55889751b9548dab10826b39
jvitoralb <jvvalb@gmail.com>
2022-08-22 22:17:22 -0300

    added comment

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ app.use('/url-shortener', shorterner);
 **/
 app.use('/exercise-tracker', exercise);
 
+/**
+ *  Need to separate app and server 
+**/
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
