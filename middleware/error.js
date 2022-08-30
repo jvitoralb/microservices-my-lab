d597d441f6c6626b2d66ab3ef2789242f7ca6930
jvitoralb <jvvalb@gmail.com>
2022-08-30 11:10:01 -0300

    minor changes

--- middleware/error.js
+++ middleware/error.js
@@ -5,7 +5,10 @@ const handleError = (err, req, res, next) => {
     if (err instanceof CustomError) {
         return res.status(err.status).json({ msg: err.message });
     }
-    return res.status(500).json({ msg: 'Something went wrong, try again later!' });
+
+    return res.status(500).json({
+        msg: 'Something went wrong, try again later!'
+    });
 }
 
 export default handleError;
