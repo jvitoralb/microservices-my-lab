76bc43998a2724ee2fb0737524ae294a822ebf26
jvitoralb <jvvalb@gmail.com>
2023-01-27 00:13:41 -0300

    upload new structure metadata

---
+++ backend/components/metadata/metadataMiddleware.js
@@ -0,0 +1,11 @@
+import CustomError from '../../errors/custom.js';
+
+
+const checkUpload = (req, res, next) => {
+    if(!req.file) {
+        throw new CustomError('Something is missing!', 404);
+    }
+    next();
+}
+
+export default checkUpload;
